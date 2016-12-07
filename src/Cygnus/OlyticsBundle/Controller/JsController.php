1737aa04e3a2000391cdb8676e43221eee85ae08
Josh Worden <solocommand@gmail.com>
2016-12-07 09:59:58 -0600

    Cache building of javascript files, gzipping

--- src/Cygnus/OlyticsBundle/Controller/JsController.php
+++ src/Cygnus/OlyticsBundle/Controller/JsController.php
@@ -3,6 +3,7 @@
 namespace Cygnus\OlyticsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use \DateTime;
 use \DateTimeZone;
@@ -10,25 +11,59 @@ use \JSMin;
 
 class JsController extends Controller
 {
-
+    /**
+     *
+     */
     const RESOURCE_LOC = '@CygnusOlyticsBundle/Resources/public/js';
 
+    /**
+     *
+     */
     const EXPIRES = 7200;
 
-    protected $files = ['uuid.js', 'json2.js', 'olytics.js'];
+    /**
+     * List of files that should be included in compiled javascript file.
+     * @var array
+     */
+    protected $files = ['uuid.js', 'json2.js'];
 
-    public function sapienceAction()
+    /**
+     * Handles requests to load sapience.js or olytics.js
+     *
+     * @param   string      $file       The file being requested
+     * @param   Request     $request
+     * @return  Response
+     */
+    public function indexAction($file, Request $request)
     {
-        $this->files = ['uuid.js', 'json2.js', 'sapience.js'];
-        return $this->indexAction();
+        $this->files[] = sprintf('%s.js', $file);
+
+        $minify = 'prod' === $this->getParameter('kernel.environment');
+
+        $cacheFilename = sprintf(
+            '_cache/%s%s.js',
+            $file,
+            $minify ? '.min' : ''
+        );
+
+        if (!file_exists($cacheFilename)) {
+            $this->buildFile($cacheFilename, $minify);
+        }
+
+        return $this->buildResponse(file_get_contents($cacheFilename), filemtime($cacheFilename), $minify);
     }
 
-    public function indexAction()
+    /**
+     * Builds and stores the Javascript file contents
+     *
+     * @param   string  $cacheFilename  The file that should be stored
+     * @param   bool    $minify
+     */
+    protected function buildFile($cacheFilename, $minify)
     {
-        $kernel = $this->get('kernel');
-
         $js = array();
         $lastModified = 0;
+        $kernel = $this->get('kernel');
         foreach ($this->files as $file) {
             $fileLocation = self::RESOURCE_LOC . '/' . $file;
             $path = $kernel->locateResource($fileLocation);
@@ -42,16 +77,29 @@ class JsController extends Controller
             }
         }
 
+        $content = implode($js, "\r\n");
+        if (true === $minify) {
+            $content = JSMin::minify($content);
+        }
+
+        file_put_contents($cacheFilename, $content);
+        return $this->buildResponse($content, $lastModified, $minify);
+    }
+
+    /**
+     * Returns a response object
+     *
+     * @param   string  $content        The file contents being returned
+     * @param   int     $lastModified   The last modified date of the contents
+     * @return  Response
+     */
+    protected function buildResponse($content, $lastModified)
+    {
         $modified = new DateTime();
         $modified->setTimestamp($lastModified);
 
         $expires = new DateTime();
         $expires->setTimestamp(time() + self::EXPIRES);
-
-        $content = implode($js, "\r\n");
-        if ('prod' === $this->getParameter('kernel.environment')) {
-            $content = JSMin::minify($content);
-        }
 
         $headers = array(
             'Content-Type'      => 'text/javascript',
